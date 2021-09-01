e98e8c0b6f6110b82cbdcd49f80f763357ec0ab8
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-31 21:26:44 -0400

    Add get_player_guess to Player class

--- main.rb
+++ main.rb
@@ -8,3 +8,25 @@
     # - If entire array guessed correctly computer_code_array[] == player_guess_array[]
         # - Player wins
 #After a round player_guess_array[] resets and game goes back to line 4
+
+class Player 
+
+  def initialize(player_guess_array=[], player_guess='blue')
+    @player_guess_array = player_guess_array
+    @player_guess = player_guess
+  end
+
+  def get_player_guess
+    print "What is your guess for the first index of the computer code?"
+    @player_guess = gets
+    p @player_guess.chomp
+  end
+
+end
+
+#class Computer
+
+#end
+
+first_player_turn = Player.new
+first_player_turn.get_player_guess
