b591b815e1ecd310ff180f7b60d97804189562b5
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-31 23:57:04 -0400

    Add check_for_gameover

--- main.rb
+++ main.rb
@@ -1,14 +1,3 @@
-
-#To start this game should play 1 round of player guessing a computer's random code
-#Computer randomly selects secret colors into computer_code_array[]
-#Player guesses color for first index and it goes into player_guess_array[]
-#Player receives feedback about how good a guess was
-    # - If guess was correct exactly, or just correct color but wrong space
-        # - array[index] == guess or array.include?(color)
-    # - If entire array guessed correctly computer_code_array[] == player_guess_array[]
-        # - Player wins
-#After a round player_guess_array[] resets and game goes back to line 4
-
 class Player 
 
   attr_accessor :player_guess
@@ -44,15 +33,21 @@ class Player
       puts "This is turn # #{turn+1}. "
       play_single_turn(computer_array) 
       puts @player_guess_array
-      if @player_guess_array == computer_array
-        puts "You have correctly guessed the computer's entire code!"
-      else 
-        puts "You couldn't guess the code this time around, but let's try again starting at the first color. "
-        @player_guess_array = []
-      end
+      check_for_gameover(@player_guess_array, computer_array)
     end
+    puts "Looks like you couldn't get the code after 12 turns, better luck next time!"
   end
 
+  def check_for_gameover(player_array, computer_array)
+    if player_array == computer_array
+      puts "You have correctly guessed the computer's entire code!"
+      exit
+    else 
+      puts "You couldn't guess the code this time around, but let's try again starting at the first color. "
+      player_array = []
+    end
+  end
+    
 end
 
 class Computer
